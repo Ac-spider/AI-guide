cadae3434b73712e08aa456757ad4ae462a7145c
LvxSeraph <67893905+Ac-spider@users.noreply.github.com>
2025-09-06 23:01:46 +0800

    Add files via upload

---
+++ Data Structure/Harsha Suryanarayana/link-insert pro.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+struct Node{
+    int data;
+    Node* next;
+};
+Node* head = NULL;
+void Insert(int x, int n){
+    Node* temp = new Node();
+    temp -> data = x;
+    temp -> next = NULL;
+    if (n == 1) {
+        temp -> next = head;
+        head = temp; 
+        return;
+    }
+    Node* temp2 = head;
+    for (int i = 0 ; i < n-2 ; i++){
+        temp2 = temp2 -> next;
+    }
+    temp -> next = temp2 -> next;
+    temp2 -> next = temp;
+          
+    
+
+}
+void Print(){
+    Node* temp = head;
+    printf("The linked list is: ");
+    while (temp!= NULL){
+        printf("%d ",temp -> data);
+        temp = temp -> next;
+    }
+    printf("\n");
+}
+int main(){
+    Insert(5,1); //5
+    Insert(2,2); //5,2
+    Insert(3,1); //3,5,2
+    Insert(4,3); //3,5,4,2
+
+    Print();
+}
