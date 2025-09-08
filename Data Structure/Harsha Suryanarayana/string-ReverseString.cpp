84f2435e628c9185922019a5a73aa1a5895390e1
LvxSeraph <67893905+Ac-spider@users.noreply.github.com>
2025-09-08 13:49:06 +0800

    Add files via upload

---
+++ Data Structure/Harsha Suryanarayana/string-ReverseString.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stack>
+#include <cstring>
+
+using namespace std;
+void Reverse (char *C,int n){
+    stack<char> S;
+    for(int i =0 ; i < n ; i++){
+        S.push(C[i]);
+    }
+    for(int i = 0 ; i < n ; i++){
+        C[i] = S.top();
+        S.pop();
+    }
+}
+
+int main(){
+    char C[101];
+    printf("Enter A String:");
+    gets(C);
+    Reverse(C,strlen(C));
+    printf("%s",C);
+}
